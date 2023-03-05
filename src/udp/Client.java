1120fbc9e7e90a3250384203c1bb1f36836d22a6
MIK <mminiom76@gmail.com>
2023-03-05 17:55:29 +0100

    Libro

--- src/udp/Client.java
+++ src/udp/Client.java
@@ -4,10 +4,50 @@
  */
 package udp;
 
+import java.io.*;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author M1K
  */
 public class Client {
+    
+    public static void main(String args[]) throws SocketException, IOException
+    {
+        try {
+            int portaServer = 6789;
+            InetAddress IPServer = InetAddress.getByName("localhost");
+            
+            byte[] bufferOUT = new byte[1024];
+            byte[] bufferIN = new byte[1024];
+            BufferedReader input = new BufferedReader(new InputStreamReader (System.in));
+            
+            DatagramSocket clientSocket;
+            clientSocket = new DatagramSocket();
+        
+            System.out.println("Client pronto - inserisci un dato da inviare:");
+            
+            String daSpedire = input.readLine();
+            bufferOUT = daSpedire.getBytes();
+            
+            DatagramPacket sendPacket = new DatagramPacket(bufferOUT, bufferOUT.length, IPServer, portaServer);
+            clientSocket.send(sendPacket);
+            
+            DatagramPacket receivePacket = new DatagramPacket(bufferIN, bufferIN.length);
+            clientSocket.receive(receivePacket);
+            String ricevuto = new String(receivePacket.getData());
+            
+            int numCaratteri = receivePacket.getLength();
+            ricevuto=ricevuto.substring(0,numCaratteri);
+            System.out.println("dal SERVER:" + ricevuto);
+            
+            clientSocket.close();
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
 }
--- src/udp/Server.java
+++ src/udp/Server.java
@@ -4,10 +4,8 @@
  */
 package udp;
 
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.SocketException;
+import java.io.*;
+import java.net.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,29 +14,51 @@ import java.util.logging.Logger;
  * @author M1K
  */
 public class Server {
-    
-    public static void main(String[] args) {
-        DatagramSocket serverSocket = null;
+
+    public static void main(String[] args){
         try {
-            serverSocket = new DatagramSocket(6666);
+            DatagramSocket serverSocket = new DatagramSocket(6789);
+            boolean attivo = true;
+            byte[] bufferIN = new byte[1024];
+            byte[] bufferOUT = new byte[1024];
+            
+            System.out.println("SERVER avviato...");
+            while(attivo)
+            {
+                DatagramPacket receivePacket = new DatagramPacket(bufferIN,bufferIN.length);
+                try {
+                    serverSocket.receive(receivePacket);
+                } catch (IOException ex) {
+                    Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                String ricevuto = new String(receivePacket.getData());
+                int numCaratteri = receivePacket.getLength();
+                ricevuto=ricevuto.substring(0,numCaratteri);
+                System.out.println("RICEVUTO: "+ricevuto);
+                
+                InetAddress IPClient = receivePacket.getAddress();
+                int portaClient = receivePacket.getPort();
+                
+                String daSpedire = ricevuto.toUpperCase();
+                bufferOUT = daSpedire.getBytes();
+                
+                DatagramPacket sendPacket = new DatagramPacket(bufferOUT, bufferOUT.length, IPClient, portaClient);
+                try {
+                    serverSocket.send(sendPacket);
+                } catch (IOException ex) {
+                    Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+                if(ricevuto.equals("fine"))
+                {
+                    System.out.println("SERVER IN CHIUSURA. Buona serata.");
+                    attivo=false;
+                }
+            }
+            serverSocket.close();
         } catch (SocketException ex) {
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
         }
-        boolean attivo = ("zioPera" == "zioPera");
-        byte[] bufferIN = new byte[1024];
-        byte[] bufferOUT = new byte[1024];
-        
-        System.out.println("SERVER avviato...");
-        while(attivo){
-            DatagramPacket receivePacket = new DatagramPacket(bufferIN, bufferIN.length);
-            try {
-                serverSocket.receive(receivePacket);
-            } catch (IOException ex) {
-                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            String ricevuto = new String(receivePacket.getData());
-            int numCaratteri = receivePacket.getLength();
-        }
-        
     }
+    
 }
